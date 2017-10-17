e20ff0e54c6c70ba8c4fb254a7df4d35e1f75318
michi201 <mp.2000@wp.pl>
2017-10-17 12:05:10 +0200

    Pętladalej

--- c++/pętlaznak.cpp
+++ c++/pętlaznak.cpp
@@ -6,6 +6,24 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	char zn='t';
+    while (zn== 't' || zn=='T' || zn=='n' || zn=='N') {
+        cout << "Podaj znak :" ;
+        cin>> zn ;
+        //if (zn== 't' || zn=='T' || zn=='n' || zn=='N')
+            //cout << zn << endl ;
+    //else
+            //break ;
+    //switch (zn) {
+        //case 't' :
+        //case 'T' :
+        //case 'n' :
+        //case 'N' :
+        //cout << zn ;
+        //break ;
+        //default:
+        //cout 
+            //break;
+};
     
     
     
