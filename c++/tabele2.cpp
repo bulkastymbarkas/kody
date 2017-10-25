dc1f783cef6b556d3ad32d956aead53ca78bc2da
michi201 <mp.2000@wp.pl>
2017-10-25 12:34:40 +0200

    tablice

--- c++/tabele2.cpp
+++ c++/tabele2.cpp
@@ -2,11 +2,36 @@
 
 
 #include <iostream>
-
+#include <cmath>
 
 using namespace std;
 
+int sumuj(int tab[], int ile) {
+    int suma=0;
+    cout << "Podane oceny :"  << ile << endl ;
+    for(int i = 0; i < ile; i++ ) 
+    {
+    suma +=tab[i];   
+    cout <<tab[i] <<" ";
+    }
+    return suma;
+}
 
+void pobierzDane(int tab[], int ile) {
+     
+    cout <<"Podaj :" << ile << "liczb:" << endl ;
+    for(int i = 0; i < ile; i++ ) 
+    {
+    cin >> tab [i];
+    }
+    
+    
+    }
+    
+float liczSrednia(int tab[],int ile ) {
+    return sumuj(tab, ile)/float(ile);
+}
+       
 int main(int argc, char **argv)
 {
     //const int rozmiar = 5;
@@ -15,24 +40,26 @@ int main(int argc, char **argv)
     cin >> rozmiar ;
     
     int liczby[rozmiar];
-    int suma = 0 ;
     
-    cout <<"Podaj pięć ocen (0-6) :" << endl ;
-    for(int i = 0; i < rozmiar; i++ ) 
-    {
-    cin >> liczby [i];
-    }
+    pobierzDane(liczby, rozmiar);
     
+    //~cout <<"Podaj pięć ocen (0-6) :" << endl ;
+    //~for(int i = 0; i < rozmiar; i++ ) 
+    //~{
+    //~cin >> liczby [i];
+    //~}
+     
     
-    cout << "Podane oceny :" << endl ;
-    for(int i = 0; i < rozmiar; i++ ) 
-    {
-    suma +=liczby[i]  ;   
+    //~cout << "Podane oceny :" << endl ;
+    //~for(int i = 0; i < rozmiar; i++ ) 
+    //~{
+    //~suma +=liczby[i]  ;   
     
-    cout <<liczby[i] << endl ;
-    }
-    cout <<"suma ocen :" << suma <<endl ;
-    cout <<"Średnia arytmetyczna:" << float (suma)/float (5) << endl ;
+    //~cout <<liczby[i] << endl ;
+    //~}
+    cout <<"suma ocen :" << sumuj(liczby, rozmiar) <<endl ;
+    //~cout <<"Średnia arytmetyczna:" << float (suma)/float (5) << endl ;
+    liczSrednia (liczby, rozmiar) << endl ;
 	return 0;
 }
 
