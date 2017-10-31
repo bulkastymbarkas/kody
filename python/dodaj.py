02bd36562ca3bebed7445c02d506251ab0f2cc56
michi201 <mp.2000@wp.pl>
2017-10-31 13:30:50 +0100

    python

---
+++ python/dodaj.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def odejmij(x, y):
+    return x - y
+def sumuj(x, y):
+    return x + y
+    
+    
+
+a = int (input('PODAJ LICZBE\n'))
+b = int (input('PODAJ LICZBE\n'))
+print ("suma:",sumuj(a,b))
+print ("Różnica:",odejmij(a,b))
---
+++ python/witaj.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#osoba = "Krystian Paluch"
+#osoba = 'Krystian Paluch'
+osoba = input ('Jak sie nazywasz?\n')
+print("Witaj",osoba,"!")
+
+rok= 2017
+wiek =  int (input('Ile masz lat\n'))
+print('Urodziłeś sie w',rok - wiek,'roku')
