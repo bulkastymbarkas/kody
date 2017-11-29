467d8f950da90acf08c4a1079629e15d646d4eaf
Szczebrzeszyn <krystian.p20@interia.pl>
2017-11-29 12:35:55 +0100

    fibo

--- python/fibonacchi.py
+++ python/fibonacchi.py
@@ -11,18 +11,33 @@ def fib_iter(n):
     """
     a, b = (0, 1)
     if n == 0:
-        print a
+        print (a)
         return a
-    elif n == 1:
-        print b
-        return b
-
+    # elif n == 1:
+     #   print (b)
+      #  return b
+    print(a)
+    print(b)
     for i in range(2, n):
-        pass
-        return b
+        tmp = b
+        b = a + b
+        a = tmp
+        print(a, " Wyraz ", i + 1, ":  ", b, "Iloraz: ", b / a)
+    return b
+
+
+def fib_iter2(n):
+    a, b = (0, 1)
+    print(a)
+    while n > 0:
+        a, b = b, a + b
+        print(a, " ", b, "Iloraz: ", b / a)
+        n = n - 1
 
 
 def main(args):
+    fib_iter2(10)
+
     return 0
 
 
