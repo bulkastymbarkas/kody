d20f31b3cbc83cc3881278079e3f6ba35558c197
bulkastymbarkas <31763275+bulkastymbarkas@users.noreply.github.com>
2018-09-10 14:11:42 +0200

    Add files via upload

---
+++ c++/bmi.cxx
@@ -0,0 +1,31 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    float masa=0;
+    float wzrost=0;
+    float bmi=0;
+    
+    while
+    {
+    
+    cout << "Podaj swoją masę: ";
+    cin >> masa;
+    cout << "Podaj swój wzrost w centymetrach: ";
+    cin >> wzrost;
+    bmi=masa/(wzrost*wzrost)*10000;
+    
+    if (18.5 >bmi)
+    {
+        cout << "niedowaga";
+    }   
+    if (18.5 <=bmi && bmi )
+    {
+        cout
+	return 0;
+}
+
