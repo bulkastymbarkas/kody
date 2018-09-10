84b8cb6e7e4aeda3ef95f885af216fe13f5c2dfc
bulkastymbarkas <31763275+bulkastymbarkas@users.noreply.github.com>
2018-09-10 14:11:19 +0200

    Add files via upload

---
+++ python/bmi.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main(args):
+    masa = float(input("podaj masę(kg"))
+    print(masa)
+    return 0
+
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
